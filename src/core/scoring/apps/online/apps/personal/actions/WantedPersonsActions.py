243e7b3d985570da494dc612a88ea35610effb60
Pavel Tiron <pavel.tiron@simzirok.com>
2014-07-06 22:53:57 +0300

    daily changes

---
+++ src/core/scoring/apps/online/apps/personal/actions/WantedPersonsActions.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from core.main.base.actions.BaseActions import BaseActions
+from django.utils.translation import ugettext_lazy as _
+
+
+class WantedPersonsActions(BaseActions):
+    pass
