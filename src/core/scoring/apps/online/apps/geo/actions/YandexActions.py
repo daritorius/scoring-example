d603afd824948a1e3f9bbb1a36a2e2544e0cddd0
Pavel Tiron <pavel.tiron@gmail.com>
2014-07-08 19:01:45 +0300

    update yandex geo api

--- src/core/scoring/apps/online/apps/geo/actions/YandexActions.py
+++ src/core/scoring/apps/online/apps/geo/actions/YandexActions.py
@@ -1,18 +1,91 @@
 # -*- coding: utf-8 -*-
+import json
+import urllib
+import urllib2
 from core.main.base.actions.BaseOnlineScoringActions import BaseOnlineScoringActions
 from django.utils.translation import ugettext_lazy as _
 
 
 class YandexActions(BaseOnlineScoringActions):
     url = 'http://geocode-maps.yandex.ru/1.x/'
+    port = 80
+    base_path = '/'
     query = 'geocode'
+    format = 'json'
+    method = 'GET'
 
     def check_address(self, data):
-        pass
+        self.check_official_address(data)
+        self.check_real_address(data)
 
     def check_official_address(self, data):
-        city = getattr(data.profile_official_address, 'official_city') if \
-            hasattr(data.profile_official_address, 'official_city') else None
+        query = self.make_query(data, address_type='official')
+        query_data = {'geocode': query, 'format': self.format}
+        geo_data = self.make_connection(query_data)
+        try:
+            count = geo_data['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found']
+            if count:
+                print 'found in geo yandex: %s' % count
+        except Exception as e:
+            print e
+            pass
 
     def check_real_address(self, data):
-        pass
+        query = self.make_query(data, address_type='real')
+        query_data = {'geocode': query, 'format': self.format}
+        print query_data
+        geo_data = self.make_connection(query_data)
+        try:
+            count = geo_data['response']['GeoObjectCollection']['metaDataProperty']['GeocoderResponseMetaData']['found']
+            if count:
+                print 'found in geo yandex: %s' % count
+        except Exception as e:
+            print e
+            pass
+
+    def make_connection(self, data):
+        try:
+            if data is not None:
+                data = urllib.urlencode(data)
+            result = json.loads(urllib2.urlopen(self.url + self.base_path + '?' + data).read())
+            return result
+        except Exception as e:
+            print e
+            return None
+
+    @staticmethod
+    def make_query(data, address_type='official'):
+        query = ''
+        if address_type == 'real':
+            country = getattr(data.profile_real_address, '%s_country' % address_type) if \
+                hasattr(data.profile_real_address, '%s_country' % address_type) else None
+            city = getattr(data.profile_real_address, '%s_city' % address_type) if \
+                hasattr(data.profile_real_address, '%s_city' % address_type) else None
+            street = getattr(data.profile_real_address, '%s_street' % address_type) if \
+                hasattr(data.profile_real_address, '%s_street' % address_type) else None
+            house = getattr(data.profile_real_address, '%s_house' % address_type) if \
+                hasattr(data.profile_real_address, '%s_house' % address_type) else None
+        else:
+            country = getattr(data.profile_official_address, '%s_country' % address_type) if \
+                hasattr(data.profile_official_address, '%s_country' % address_type) else None
+            city = getattr(data.profile_official_address, '%s_city' % address_type) if \
+                hasattr(data.profile_official_address, '%s_city' % address_type) else None
+            street = getattr(data.profile_official_address, '%s_street' % address_type) if \
+                hasattr(data.profile_official_address, '%s_street' % address_type) else None
+            house = getattr(data.profile_official_address, '%s_house' % address_type) if \
+                hasattr(data.profile_official_address, '%s_house' % address_type) else None
+        ## make query
+        query += '%s' % country if country else ''
+        if len(query) and city:
+            query += ',+%s' % city
+        elif not len(query) and city:
+            query += '%s' % city
+        if len(query) and street:
+            query += ',+%s' % street
+        elif not len(query) and street:
+            query += '%s' % street
+        if len(query) and house:
+            query += ',+%s' % house
+        elif not len(query) and house:
+            query += '%s' % house
+        return query
--- src/core/scoring/apps/online/apps/geo/management/commands/check_yandex_geo.py
+++ src/core/scoring/apps/online/apps/geo/management/commands/check_yandex_geo.py
@@ -1,12 +1,49 @@
 # -*- coding: utf-8 -*-
+from django.utils.translation import ugettext_lazy as _
+import json
+import urllib
+import urllib2
+from core.main.base.modules.Singleton import Singleton
 from core.scoring.apps.online.apps.geo.actions.YandexActions import YandexActions
 from django.core.management import BaseCommand
-from django.utils.translation import ugettext_lazy as _
+
+
+class Data(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        setattr(self, 'profile_official_address', OfficialAddress())
+        setattr(self, 'profile_real_address', RealAddress())
+
+
+class OfficialAddress(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        setattr(self, 'official_country', 'Украина')
+        setattr(self, 'official_city', 'Киев')
+        setattr(self, 'official_street', '1 мая')
+        setattr(self, 'official_house', '19')
+
+
+class RealAddress(object):
+    __metaclass__ = Singleton
+
+    def __init__(self):
+        setattr(self, 'real_country', 'Украина')
+        setattr(self, 'real_city', 'Киев')
+        setattr(self, 'real_street', '1 мая')
+        setattr(self, 'real_house', '19')
 
 
 class Command(BaseCommand):
     yandex_actions = YandexActions()
 
+    @staticmethod
+    def generate_data():
+        data = Data()
+        return data
+
     def handle(self, *args, **options):
-        data = {}
+        data = self.generate_data()
         self.yandex_actions.check_address(data)
